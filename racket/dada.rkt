bb675e0cd610fb10abd4a0c69ba7ad65fe763c98
Niko Matsakis <niko@alum.mit.edu>
2021-07-25 03:48:32 +0300

    start poking on a racket model

---
+++ racket/dada.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require redex)
+(require "grammar.rkt")
+
+;; TODO
+;;
+;; * Generics
+;; * ref classes/structs
+;; * ref types
+;; * interfaces and dyn types
+;; * forall types
+;; * existential types
---
+++ racket/grammar.rkt
@@ -0,0 +1,47 @@
+#lang racket
+(require redex)
+(provide (all-defined-out))
+
+;; TODO
+;;
+;; * Generics
+;; * ref classes/structs
+;; * ref types
+;; * interfaces and dyn types
+;; * forall types
+;; * existential types
+
+(define-language Dada
+  (program (class-definition ...) (struct-definition ...) (method-defn ...))
+  (class-definition (class c (field-decl ...)))
+  (struct-definition (struct s (field-decl ...)))
+  (method-definition (fn m (var-decl ...) ty expr))
+  (var-decl (x ty))
+  (field-decl (f ty))
+  (ty (mode c)
+      s
+      int
+      (mode ty))
+  (mode my own (shared (origin ...)) (borrowed (origin ...)))
+  (access my own shared borrowed)
+  (origin o)
+  (expr (let var-decl = expr)
+        (set place = expr)
+        (call f (expr ...))
+        (access place)
+        number
+        (seq expr ...)
+        (dead x))
+  (place (x f ...))
+  (x variable-not-otherwise-mentioned)
+  (m variable-not-otherwise-mentioned)
+  (o variable-not-otherwise-mentioned)
+  (s variable-not-otherwise-mentioned)
+  (f variable-not-otherwise-mentioned)
+  (c variable-not-otherwise-mentioned))
+
+;; I can't figure out how to write these as real racket unit tests.
+
+;(test-equal (redex-match Dada expr '(dead x)) (list '(match (bind 'expr '(dead x)))))
+; (redex-match Dada expr '(seq (dead x)))
+; (redex-match Dada expr '(seq (let (x int) = 22) (set (x) = 23) (call foo ((my (x)))) (dead x)))
