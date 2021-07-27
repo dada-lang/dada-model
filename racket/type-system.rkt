d2aa2a67610d0b0c655518333cb8b436412b0e5f
Niko Matsakis <niko@alum.mit.edu>
2021-07-27 05:42:19 +0300

    tests for applying modes

--- racket/type-system.rkt
+++ racket/type-system.rkt
@@ -78,10 +78,12 @@
 (test-equal (term (combine-access-modes (copy clone clone move))) (term move))
 (test-equal (term (combine-access-modes (copy clone clone reborrow))) (term reborrow))
 
-;; `(ty-access-mode program ty)`: After a value of type `ty` is
-;; "moved" with `my`, what operation is needed? This is determined
-;; by walking the type recursively to determine what sort of classes
-;; are found within.
+;; ty-access-mode program ty - access-mode
+;;
+;; After a value of type `ty` is "moved" with `my`,
+;; what operation is needed? This is determined
+;; by walking the type recursively to determine what
+;; sort of classes are found within.
 ;;
 ;; - `move` -- remove the old value
 ;; - `borrow` -- reborrow existing 
@@ -125,19 +127,25 @@
   (test-equal (term (ty-access-mode ,program move-struct)) (term move))
   )
 
-;; definitely-initialized env place -- holds if place is definitely initialized
+;; definitely-initialized env place -> boolean
+;;
+;; True if place is definitely initialized
 (define-metafunction dada-type-system
   definitely-initialized : env place -> boolean
   [(definitely-initialized env place)
    (place-or-prefix-in place (definitely-initialized-places env))])
 
-;; maybe-initialized env place -- holds if place may be initialized
+;; maybe-initialized env place -> boolean
+;;
+;; True if place may be initialized
 (define-metafunction dada-type-system
   maybe-initialized : env place -> boolean
   [(maybe-initialized env place)
    (place-or-prefix-in place (maybe-initialized-places env))])
 
-;; definitely-not-sinitialized env place -- holds if place is definitely initialized
+;; definitely-not-initialized env place -> boolean
+;;
+;; True if place is definitely initialized
 (define-metafunction dada-type-system
   definitely-not-initialized : env place -> boolean
   [(definitely-not-initialized env place)
@@ -165,13 +173,39 @@
   [(merge-origins origins_1 origins_2)
    ,(sort (remove-duplicates (append (term origins_1) (term origins_2))) place<?)])
 
-;; merge-mode -- find the GLB on the lattice:
+;; merge-mode mode_1 mode_2 -> mode
+;;
+;; Find find the GLB on the lattice, where a -> b means a >= b
 ;;
 ;; my      --->    our
 ;; |                |
 ;; v                v
 ;; borrowed -->  shared
-;;        
+;;
+;; The lattice ordering is that mode_1 >= mode_2 if
+;;
+;;     "having a value in mode_1 gives you more capability
+;;      than a value in mode_2"
+;;
+;; or
+;;
+;;     "if a program is legal with a value in mode_2,
+;;      then it is legal if that value is in mode_1"
+;;
+;; basically Liskov's substitution principle
+;; (i.e., a value in mode_2 <: a value in mode_1).
+;;
+;;
+;; Examples and notes:
+;;
+;; * `my` is the greatest because you can do anything if you have unique
+;;   ownership
+;; * `our` and `(borrowed origins)` have no direct ordering because:
+;;   - `our` doesn't permit mutating `mut` fields (whereas `borrowed` does)
+;;   - but `borrowed origins` doesn't allow creating an `our`, as you lack ownership
+;;     - `(borrowed origins)` also forbids access to `origins` (which may be an empty set, though)
+;; * `(borrowed origins_1) >= (borrowed origins_2)` if `origins_1 <= origins_2`
+;;   - because `(borrowed ())` imposes no restrictions on what you can do with other variables
 (define-metafunction dada-type-system
   merge-mode : mode mode -> mode
   [(merge-mode my mode) mode]
@@ -191,6 +225,11 @@
   [(merge-mode (shared origins_1) (shared origins_2)) (shared (merge-origins origins_1 origins_2))]
   )
 
+;; apply-mode program mode ty
+;;
+;; Applying a mode to a type means converting to the 'least mode'
+;; of the mode in the type already. So e.g. a "shared" version of
+;; a "borrowed" class is a "shared class".
 (define-metafunction dada-type-system
   apply-mode : program mode ty -> ty
   [(apply-mode _ _ int) int]
@@ -211,5 +250,13 @@
   ;; we could actually do better here, because `(shared x)` subsumes `(shared (x y))`
   (test-equal (term (merge-origins ((shared (x)) (shared (z))) ((shared (z)) (shared (x y))))) (term ((shared (x)) (shared (x y)) (shared (z)))))
 
+  ;; we could actually do better here, because `(shared x)` subsumes `(borrowed x)`
+  (test-equal (term (merge-origins ((shared (x))) ((borrowed (x))))) (term ((borrowed (x)) (shared (x)))))
+
   (test-equal (term (apply-mode ,program (shared ((shared (x)))) (my the-class))) (term ((shared ((shared (x)))) the-class)))
+
+  ;; Here: it's important that origins carry an origin-kind,
+  ;; because we have to remember that the shared reference came from a
+  ;; `borrowed (y)`!
+  (test-equal (term (apply-mode ,program (shared ((shared (x)))) ((borrowed ((borrowed (y)))) the-class))) (term ((shared ((borrowed (y)) (shared (x)))) the-class)))
   )
