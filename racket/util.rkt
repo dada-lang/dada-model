d59db41f242dec9b8643743c6cd786a3abf721a5
Niko Matsakis <niko@alum.mit.edu>
2021-08-01 23:09:26 +0300

    extend invalidation to cover transitive cases
    We used to just invalidate the value that was written.
    Now we also invalidate values that get invalidated!

--- racket/util.rkt
+++ racket/util.rkt
@@ -4,3 +4,4 @@
 
 (define-syntax-rule (test-equal-terms a b) (test-equal (term a) (term b)))
 (define-syntax-rule (log name value) (begin (pretty-print '(entering name value)) (let [(v value)] (pretty-print (list 'exiting name v)) value)))
+(define (partition-list f l) (let-values [((matches matches-not) (partition f l))] (list matches matches-not)))
