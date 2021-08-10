208f107f673b4013fe2d5bdf863be448fead1bfd
Niko Matsakis <niko@alum.mit.edu>
2021-08-10 14:17:31 +0300

    add accessible.rkt

---
+++ racket/type-system/accessible.rkt
@@ -0,0 +1,153 @@
+#lang racket
+(require redex "../grammar.rkt" "../util.rkt" "lang.rkt" "lease-implication.rkt")
+(provide place-accessible)
+
+;; assignable program env ty_source ty_target
+;;
+;; Holds if a value of type `ty_source` can be assigned to a
+;; place of type `ty_target`.
+(define-judgment-form dada-type-system
+  #:mode (place-accessible I I I I O)
+  #:contract (place-accessible program_in env_in action-kind place_in atomic?)
+  #:inv (all? (defined? (place-ty program_in env_in place_in)))
+
+  ; Any sort of access to a local variable is always legal
+  [--------------------------
+   (place-accessible program env action-kind (x) ())]
+
+  [(place-accessible program env read (x_0 f_0 ...) atomic?)
+   (side-condition (unique-value? program env (x_0 f_0 ...)))
+   --------------------------
+   (place-accessible program env read (x_0 f_0 ... f_1) atomic?)
+   ]
+
+  ; Accessing an atomic field of a shared value
+  ; requires an atomic section (true for read or write).
+  [(where place_0 (x_0 f_0 ...))
+   (where (mode_0 c_0 _) (place-ty program env place_0))
+   (where #t (any? (shared-place? program env place_0) (shared-mode? mode_0)))
+   (where atomic (class-field-mutability program c_0 f_1))
+   --------------------------
+   (place-accessible program env _ (x_0 f_0 ... f_1) (atomic))
+   ]
+
+  ; Reading a non-atomic field is legal whenever reading the
+  ; prefix is legal.
+  [(where place_0 (x_0 f_0 ...))
+   (where (_ c _) (place-ty program env place_0))
+   (side-condition (class-field-non-atomic? program c f_1))
+   (place-accessible program env read place_0 atomic?)
+   --------------------------
+   (place-accessible program env read (x_0 f_0 ... f_1) atomic?)
+   ]
+
+  ; Writing a var field is legal whenever writing the
+  ; prefix is legal.
+  [(where place_0 (x_0 f_0 ...))
+   (where (my c _) (place-ty program env place_0))
+   (where var (class-field-mutability program c f_1))
+   (place-accessible program env write place_0 atomic?)
+   --------------------------
+   (place-accessible program env write (x_0 f_0 ... f_1) atomic?)
+   ]
+
+  
+  )
+
+;; shared-place? program env place
+;;
+;; True if the place reached by P is (potentially) reachable via
+;; some other, independent place P_1.
+;;
+;; Two places P_0, P_1 are *independent* if using P_0 does not
+;; invalidate using P_1 and vice-versa.
+(define-metafunction
+  dada-type-system
+  shared-place? : program env place -> boolean
+
+  [(shared-place? program env (x f_0 ... f_1 f_2 ...))
+   ; Accessing a field of a shared value is shared.
+   #t
+   (where place_0 ((x f_0) ...))
+   (where ((shared _) c _) (place-ty program env place_0))
+
+   or
+
+   ; Accessing a shared field of a value is shared.
+   #t
+   (where place_0 ((x f_0) ...))
+   (where (mode c _) (place-ty program env place_0))
+   (where #t (class-field-shared? program c f_1))
+   ]
+
+  
+  [(shared-place? _ _ _)
+   ; Everything else is unique.
+   #f]
+  
+  )
+  
+(redex-let*
+ dada-type-system
+ [(program program_test)
+
+  (ty_my_character (term (my Character ())))
+  
+  (ty_my_string (term (my String ())))
+  (ty_my_pair_of_my_strings (term (my Pair (ty_my_string ty_my_string))))
+  (ty_my_pair_char_str (term (my Pair (ty_my_character ty_my_string))))
+  
+  (ty_our_string (term (our String ())))
+  (ty_our_pair_of_my_strings (term (our Pair (ty_my_string ty_my_string))))
+  
+  (env (term (test-env (pair-ch ty_my_pair_char_str)
+                       (cell-ch (my Cell (ty_my_character)))
+                       (shvar-cell-ch (my ShVar ((my Cell (ty_my_character))))))))
+  ]
+
+ (define-syntax-rule
+   (dada-test-accessible-unatomic action-term place-term)
+   (test-judgment-holds
+    (place-accessible
+     program
+     env
+     action-term
+     place-term
+     (side-condition atomic? (equal? (term ()) (term atomic?))))))
+
+ (define-syntax-rule
+   (dada-test-accessible-atomic action-term place-term)
+   (test-judgment-holds
+    (place-accessible
+     program
+     env
+     action-term
+     place-term
+     (side-condition atomic? (equal? (term (atomic)) (term atomic?))))))
+
+ (define-syntax-rule
+   (dada-test-not-accessible action-term place-term)
+   (test-judgment-false
+    (place-accessible
+     program
+     env
+     action-term
+     place-term
+     _)))
+
+ ; the hp field is declared as var, hence can be read and written
+ #;(dada-test-accessible-unatomic read (pair-ch a hp))
+ #;(dada-test-accessible-unatomic write (pair-ch a hp))
+
+ ; the name field is declared as shared, hence can be read  but not written
+ #;(dada-test-accessible-unatomic read (pair-ch a name))
+ #;(dada-test-not-accessible write (pair-ch a name))
+
+ ; Atomic fields are accessible without atomic when unique
+ (dada-test-accessible-unatomic read (cell-ch value hp))
+ #;(dada-test-accessible-unatomic write (cell-ch value hp))
+
+ ; But not if shared
+ (dada-test-accessible-atomic read (shvar-cell-ch shv value hp))
+ #;(dada-test-accessible-atomic write (shvar-cell-ch shv value hp))
+ )
