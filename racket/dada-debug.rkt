df0d542fb8d4984e3930994566821d22fc482bf3
Niko Matsakis <niko@alum.mit.edu>
2021-08-24 04:58:11 -0400

    wip: method test

--- racket/dada-debug.rkt
+++ racket/dada-debug.rkt
@@ -2,21 +2,46 @@
 (require redex)
 (require "dada.rkt")
 
-  
-(dada-check-pass
- ; We *should* be able to track the dependency on `v`
- ; when we have something SHARED.
+
+(; fn get_first<lease l, type E>(x: shared(l) Vec<E>) -> shared(x) E {
+ ;     x[0]
+ ; }
  ;
  ; {
  ;   var v: my Vec<String> = Vec("foo");
  ;   var p: shared(v) Vec<String> = share v;
- ;   var v2 = v;
- ;   share p[0]; // type of `p` is now `shared(v2) Vec<String>`, hence valid
+ ;   var s: shared(v) String = get_first<shared(v), String>(s);
  ; }
- (seq ((var (v (my Vec ((my String ())))) = (class-instance Vec ((my String ())) ((class-instance String () ()))))
-       (var (p ((shared ((shared (v)))) Vec (((shared ((shared (v)))) String ())))) = (share (v)))
-       (var (v2 (my Vec ((my String ())))) = (give (v)))
-       (share (p value0))
-       ))
+
+ redex-let*
+ Dada
+ [(generic-decls (term ((l out) (E out))))
+  (ty_return (term ((shared ((shared (x)))) E)))
+  (ty_x (term ((shared (l)) Vec ((my E)))))
+  (named-method-definition_get-first (term (get-first (fn generic-decls ((x ty_x)) -> ty_return (give (x value0))))))
+  (program (term (program-with-methods
+                  program_test
+                  named-method-definition_get-first
+                  )))]
+ (dada-check-pass
+  (seq ((var (v (my Vec ((my String ())))) = (class-instance Vec ((my String ())) ((class-instance String () ()))))
+        (var (p ((shared ((shared (v)))) Vec (((shared ((shared (v)))) String ())))) = (share (v)))
+        (var (s ((shared ((shared (v)))) String ())) =
+             (call get-first
+                   (((shared (v))) (my String ()))
+                   ((give (s)))))
+        ))
+  )
  )
- 
+
+; fn get_first<lease l, type E>(x: shared(l) Vec<E>, y: shared(l) Vec<E>) -> shared(x|y) E {
+ ;     x[0]
+ ; }
+ ;
+ ; {
+ ;   var v: my Vec<String> = Vec("foo");
+ ;   var v2: my Vec<String> = Vec("foo");
+ ;   var p: shared(v) Vec<String> = share v;
+ ;   var q: shared(v2) Vec<String> = share v2;
+ ;   var s: shared(v|v2) String = get_first<shared(v|v2), String>(p, q);
+ ; }
