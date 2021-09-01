1353f2e0d100c2952ca5db5c50778d94497f8987
Niko Matsakis <niko@alum.mit.edu>
2021-09-01 05:07:25 -0400

    remove Value / Identity from return of clone
    Now that we are just always allocating an Address, we don't
    need to "promote" 'my' to an address

--- racket/opsem/clone.rkt
+++ racket/opsem/clone.rkt
@@ -12,14 +12,14 @@
   ;; Given a value that is to be cloned, return the new value
   ;; that should be stored both in the old and new places. This
   ;; may require adjusting ref-counts.
-  clone-value : Ref-counts Value -> (Ref-counts Value)
-  [(clone-value Ref-counts number) (Ref-counts number)]
+  clone-value : Ref-counts Value -> Ref-counts
+  [(clone-value Ref-counts number) Ref-counts]
   [(clone-value Ref-counts (Identity id Field-values))
-   (Ref-counts_1 (Identity_1 id Field-values))
-   (where (Ref-counts_1 Identity_1) (clone-identity Ref-counts Identity))]
+   Ref-counts_1
+   (where Ref-counts_1 (clone-identity Ref-counts Identity))]
   [(clone-value Ref-counts (Identity box Address))
-   (Ref-counts_1 (Identity_1 box Address))
-   (where (Ref-counts_1 Identity_1) (clone-identity Ref-counts Identity))]
+   Ref-counts_1
+   (where Ref-counts_1 (clone-identity Ref-counts Identity))]
   )
 
 (define-metafunction Dada
@@ -28,14 +28,16 @@
   ;; Given a value that is to be cloned, return the new value
   ;; that should be stored both in the old and new places. This
   ;; may require adjusting ref-counts.
-  clone-identity : Ref-counts Identity -> (Ref-counts Identity)
+  clone-identity : Ref-counts Identity -> Ref-counts
   [(clone-identity Ref-counts shared)
-   (Ref-counts shared)]
+   Ref-counts]
+  
   [(clone-identity Ref-counts (my Address))
-   (Ref-counts_1 (my Address))
+   Ref-counts_1
    (where Ref-counts_1 (increment-ref-count Ref-counts Address))]
+  
   [(clone-identity Ref-counts data)
-   (Ref-counts data)]
+   Ref-counts]
   )
 
 (module+ test
@@ -43,17 +45,17 @@
    Dada
    [(Ref-counts (term [(i0 1)]))]
 
-   (test-equal-terms (clone-identity Ref-counts (my i0)) (((i0 2)) (my i0)))
-   (test-equal-terms (clone-identity Ref-counts shared) (Ref-counts shared))
+   (test-equal-terms (clone-identity Ref-counts (my i0)) ((i0 2)))
+   (test-equal-terms (clone-identity Ref-counts shared) Ref-counts)
    )
   )
 
 (module+ test
   (redex-let*
    Dada
-   [(Ref-counts (term [(i0 1)]))
-    (Value (term ((my i0) box dummy-address)))]
+   [(Ref-counts (term [(i0 1)]))]
 
-   (test-equal-terms (clone-value Ref-counts Value) (((i0 2)) ((my i0) box dummy-address)))
+   (test-equal-terms (clone-value Ref-counts ((my i0) box dummy-address)) ((i0 2)))
+   (test-equal-terms (clone-value Ref-counts ((my i0) dummy-struct-name ())) ((i0 2)))
    )
   )
