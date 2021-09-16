a03d6422c3f841e01af433f46d324d45fdbd6a0a
Niko Matsakis <niko@alum.mit.edu>
2021-09-16 07:01:18 -0400

    wip

--- racket/dada-debug.rkt
+++ racket/dada-debug.rkt
@@ -2,16 +2,18 @@
 (require redex)
 (require "dada.rkt")
 
-(; Test lease cancellation
-dada-seq-test
-((var vec1 = (class-instance Vec (int) (22)))
-  (var vec2 = (lend (vec1)))
-  (var vec3 = (lend (vec1)))
-  (set (vec3 value0) = 44))
- [(vec1 (my box Heap-addr))
-  (vec2 expired)
-  (vec3 ((leased Lease-id1) box Heap-addr))
-  ]
- [(Heap-addr (box 1 ((class Vec) ((value0 44)))))]
- [(Lease-id1 (borrowed () Heap-addr))]
+(; Test shared lease cancellation on drop
+ dada-trace-test
+ ((var sh1 = ((class-instance ShVar ((my Vec (int))) ((class-instance Vec (int) (22)))): (our ShVar ((my Vec (int))))))
+  (var sh2 = (copy (sh1)))
+  (atomic (seq ((var v1 = (lend (sh1 shv)))
+                
+                (set (v1 value0) = 44)
+                (var v2 = (lend (sh2 shv)))
+                (set (v2 value0) = 66)
+                (set (v1 value0) = 88)
+                ))))
+ []
+ []
+ []
  0)
