8e214f188beed5d7d401943f6ee7689330af454f
Niko Matsakis <niko@alum.mit.edu>
2021-10-16 05:04:43 -0400

    improve opsem-share tests

--- racket/z-tests/opsem-lease-cancellation.rkt
+++ racket/z-tests/opsem-lease-cancellation.rkt
@@ -103,3 +103,102 @@
     (Heap-addr2 (box 1 44))]
    []
    (share  (vec2 value0)))
+
+(; * Share pair -> p
+ ; * Share p a -> q
+ ; * Write p b
+ ;
+ ; FIXME-- This cancels p which in turn cancels q.
+ ;         Is this correct? It does match Rust.
+ dada-seq-test
+ ((var pair = (class-instance Pair
+                              ((my Point ())
+                               (my Point ()))
+                              ((class-instance Point () (22 44))
+                               (class-instance Point () (66 88)))))
+  (var p = (share (pair)))
+  (var q = (share (p a)))
+  (set (pair b y) = 99)
+  )
+ ((pair (my box Heap-addr6))
+  (p expired)
+  (q expired)
+  )
+ ((Heap-addr (box 1 22))
+  (Heap-addr1 (box 1 44))
+  (Heap-addr2
+   (box
+    1
+    ((class Point) ((x (our box Heap-addr)) (y (our box Heap-addr1))))))
+  (Heap-addr3 (box 1 66))
+  (Heap-addr5
+   (box
+    1
+    ((class Point) ((x (our box Heap-addr3)) (y (our box Heap-addr7))))))
+  (Heap-addr6
+   (box 1 ((class Pair) ((a (my box Heap-addr2)) (b (my box Heap-addr5))))))
+  (Heap-addr7 (box 1 99)))
+ ()
+ the-Zero-value)
+
+(; * Share pair -> p
+ ; * Share pair a -> q
+ ; * Write p b
+ ;
+ ; Cancels p but not q.
+ dada-seq-test
+ ((var pair = (class-instance Pair
+                              ((my Point ())
+                               (my Point ()))
+                              ((class-instance Point () (22 44))
+                               (class-instance Point () (66 88)))))
+  (var p = (share (pair)))
+  (var q = (share (pair a)))
+  (set (pair b y) = 99)
+  )
+ ((pair (my box Heap-addr6))
+  (p expired)
+  (q ((shared Lease-id1) box Heap-addr2))
+  )
+ ((Heap-addr (box 1 22))
+  (Heap-addr1 (box 1 44))
+  (Heap-addr2
+   (box
+    1
+    ((class Point) ((x (our box Heap-addr)) (y (our box Heap-addr1))))))
+  (Heap-addr3 (box 1 66))
+  (Heap-addr5
+   (box
+    1
+    ((class Point) ((x (our box Heap-addr3)) (y (our box Heap-addr7))))))
+  (Heap-addr6
+   (box 1 ((class Pair) ((a (my box Heap-addr2)) (b (my box Heap-addr5))))))
+  (Heap-addr7 (box 1 99)))
+ ((Lease-id1 (shared () Heap-addr2)))
+ the-Zero-value)
+
+(; NB: Reading `point x` expires both `lent-point` and `shared-point2`
+ dada-seq-test
+ [(var point = (class-instance Point () (22 44)))
+  (var lent-point = (lend (point)))
+  (var some = (class-instance Some
+                              (((lent ((lent (point)))) Point ()))
+                              ((give (lent-point)))))
+  (var shared-some = (share (some)))
+  (var shared-point2 = (share (shared-some value)))
+  (share (point x))
+  ]
+ [(point (my box Heap-addr2))
+  (lent-point expired)
+  (some (my box Heap-addr3))
+  (shared-some ((shared Lease-id1) box Heap-addr3))
+  (shared-point2 expired)
+  ]
+ [(Heap-addr (box 2 22))
+  (Heap-addr1 (box 1 44))
+  (Heap-addr2 (box 1 ((class Point) ((x (our box Heap-addr)) (y (our box Heap-addr1))))))
+  (Heap-addr3 (box 1 ((class Some) ((value expired)))))
+  ]
+ [(Lease-id1 (shared () Heap-addr3))
+  ]
+ (our box Heap-addr))
--- racket/z-tests/opsem-share.rkt
+++ racket/z-tests/opsem-share.rkt
@@ -3,72 +3,57 @@
 (require redex
          "../dada.rkt")
 
-(; NB: Reading `point x` expires both `lent-point` and `shared-point2`
+(; Sharing an owned value => shared value
  dada-seq-test
- [(var point = (class-instance Point () (22 44)))
-  (var lent-point = (lend (point)))
-  (var some = (class-instance Some
-                              (((lent ((lent (point)))) Point ()))
-                              ((give (lent-point)))))
-  (var shared-some = (share (some)))
-  (var shared-point2 = (share (shared-some value)))
-  (share (point x))
+ [(var a = (class-instance Some
+                           ()
+                           ((class-instance Pair
+                                            ()
+                                            ((class-instance String () ())
+                                             44)))))
+  (var b = (share (a value a)))
   ]
- [(point (my box Heap-addr2))
-  (lent-point expired)
-  (some (my box Heap-addr3))
-  (shared-some ((shared Lease-id1) box Heap-addr3))
-  (shared-point2 expired)
-  ]
- [(Heap-addr (box 2 22))
-  (Heap-addr1 (box 1 44))
-  (Heap-addr2 (box 1 ((class Point) ((x (our box Heap-addr)) (y (our box Heap-addr1))))))
-  (Heap-addr3 (box 1 ((class Some) ((value expired)))))
-  ]
- [(Lease-id1 (shared () Heap-addr3))
-  ]
- (our box Heap-addr))
-
-(; Test subleasing and sharing
- ;
- ; FIXME: It's a bit debatable, but this setup won't
- ; match the static type system, at least-- the second share
- ; is derived from the first one, so when the first one
- ; gets canceled, shouldn't the second one? On the other hand,
- ; even if share is actually an owner, there isn't really
- ; a *need* to cancel the second one, but I suspect we will
- ; have to in order to make everything work.
- dada-seq-test
- ((var point1 = (class-instance Point () (22 44)))
-  (var point2 = (class-instance Point () (66 88)))
-  (var pair = (class-instance Pair
-                              ((my Point ())
-                               (my Point ()))
-                              ((give (point1))
-                               (give (point2)))))
-  (var p = (share (pair)))
-  (var q = (share (pair a)))
-  (set (pair b y) = 99)
-  )
- ((point1 expired)
-  (point2 expired)
-  (pair (my box Heap-addr6))
-  (p expired)
-  (q ((shared Lease-id1) box Heap-addr2))
-  )
- ((Heap-addr (box 1 22))
+ [(a (my box Heap-addr3))
+  (b ((shared Lease-id) box Heap-addr))]
+ [(Heap-addr (box 1 ((class String) ())))
   (Heap-addr1 (box 1 44))
   (Heap-addr2
-   (box
-    1
-    ((class Point) ((x (our box Heap-addr)) (y (our box Heap-addr1))))))
-  (Heap-addr3 (box 1 66))
-  (Heap-addr5
-   (box
-    1
-    ((class Point) ((x (our box Heap-addr3)) (y (our box Heap-addr7))))))
-  (Heap-addr6
-   (box 1 ((class Pair) ((a (my box Heap-addr2)) (b (my box Heap-addr5))))))
-  (Heap-addr7 (box 1 99)))
- ((Lease-id1 (shared () Heap-addr2)))
- the-Zero-value)
+   (box 1 ((class Pair) ((a (my box Heap-addr)) (b (our box Heap-addr1))))))
+  (Heap-addr3 (box 1 ((class Some) ((value (my box Heap-addr2))))))]
+ [(Lease-id (shared () Heap-addr))]
+ (our box the-Zero))
+
+(; Sharing an integer in a var field gives me back a fresh ref
+ dada-seq-test
+ [(var a = (class-instance Some
+                           ()
+                           ((class-instance Pair
+                                            ()
+                                            ((class-instance String () ())
+                                             44)))))
+  (var b = (share (a value b)))
+  ]
+ [(a (my box Heap-addr3))
+  (b (our box Heap-addr1))]
+ [(Heap-addr (box 1 ((class String) ())))
+  (Heap-addr1 (box 2 44))
+  (Heap-addr2
+   (box 1 ((class Pair) ((a (my box Heap-addr)) (b (our box Heap-addr1))))))
+  (Heap-addr3 (box 1 ((class Some) ((value (my box Heap-addr2))))))]
+ []
+ (our box the-Zero))
+
+(; Sharing an integer in an atomic field gives me back a fresh ref
+ dada-seq-test
+ [(var a = (class-instance Cell
+                           ()
+                           (22)))
+  (var b = (share (a value)))
+  ]
+ [(a (my box Heap-addr1))
+  (b (our box Heap-addr))]
+ [
+  (Heap-addr (box 2 22))
+  (Heap-addr1 (box 1 ((class Cell) ((value (our box Heap-addr))))))]
+ []
+ (our box the-Zero))
