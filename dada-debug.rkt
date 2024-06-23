aea3327b30e94e7038b9aa70ac1e4285de81a169
Niko Matsakis <niko@alum.mit.edu>
2024-06-23 16:41:05 -0400

    remove racket sources

--- dada-debug.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require redex/reduction-semantics
-         "racket/dada.rkt"
-         "racket/util.rkt"
-         "racket/opsem/traverse.rkt")
-
-(current-traced-metafunctions '())
-
-(dada-test-lend my                my                 shared     our)
--- racket/opsem.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket
-(require "opsem/lang.rkt"
-         "opsem/small-step.rkt")
-(provide Dada Dada-reduction)
